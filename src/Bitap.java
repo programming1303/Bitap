f18f7509865abc89b6a2154ce97ccd54673e98c8
Andrey Kalendarov <kalendarov-andre@mail.ru>
2014-04-10 21:11:46 +0400

    Insertion added.

--- src/Bitap.java
+++ src/Bitap.java
@@ -2,8 +2,8 @@ public class Bitap
 {
     public static void main(String args[])
     {
-        String text = "ATCCAATCGA";
-        String pattern = "ATCGA";
+        String text = "AATCCG";
+        String pattern = "ATCCG";
         System.out.println("Text: " + text);
         System.out.println("Pattern: "+ pattern);
         search(text, pattern);
@@ -13,7 +13,7 @@ public class Bitap
     {
         int length = pattern.length() - 1;
 
-        long M = 1, S = 3, vector[] = new long[256];
+        long M = 1, S = 3, I = 1, vector[] = new long[256];
         int i, k;
 
         for(k = 0; k < 256; k++)
@@ -29,11 +29,14 @@ public class Bitap
         for(i = 0; i < text.length(); i++)
         {
             S = (((S << 1) | 1) & vector[text.charAt(i)]) | (M << 1);
+            I = (((I << 1) | 1) & vector[text.charAt(i)]) | M;
             M = ((M << 1) | 1) & vector[text.charAt(i)];
             if(M >= Math.pow(2, length + 1))
                 System.out.println("Full Match " + (i - length + 1));
             if(S >= Math.pow(2, length + 1))
                 System.out.println("Substitution " + (i - length + 1));
+            if(I >= Math.pow(2, length + 1))
+                System.out.println("Insertion " + (i - length));
         }
     }
 }
