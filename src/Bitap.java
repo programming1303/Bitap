30db4ae7f928bf43eb1003c2f5ef7a6f5f2d2e4b
Andrey Kalendarov <kalendarov-andre@mail.ru>
2014-04-10 21:05:25 +0400

    Substitution added.

--- src/Bitap.java
+++ src/Bitap.java
@@ -2,7 +2,7 @@ public class Bitap
 {
     public static void main(String args[])
     {
-        String text = "ATCGAGCATCGA";
+        String text = "ATCCAATCGA";
         String pattern = "ATCGA";
         System.out.println("Text: " + text);
         System.out.println("Pattern: "+ pattern);
@@ -13,7 +13,7 @@ public class Bitap
     {
         int length = pattern.length() - 1;
 
-        long M = 1, vector[] = new long[256];
+        long M = 1, S = 3, vector[] = new long[256];
         int i, k;
 
         for(k = 0; k < 256; k++)
@@ -28,11 +28,12 @@ public class Bitap
 
         for(i = 0; i < text.length(); i++)
         {
-            M <<= 1;
-            M |= 1;
-            M &= vector[text.charAt(i)];
+            S = (((S << 1) | 1) & vector[text.charAt(i)]) | (M << 1);
+            M = ((M << 1) | 1) & vector[text.charAt(i)];
             if(M >= Math.pow(2, length + 1))
                 System.out.println("Full Match " + (i - length + 1));
+            if(S >= Math.pow(2, length + 1))
+                System.out.println("Substitution " + (i - length + 1));
         }
     }
 }
