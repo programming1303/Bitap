ce981438850f85174df47d8962e7bbb17d435ef0
Andrey Kalendarov <kalendarov-andre@mail.ru>
2014-04-10 21:17:38 +0400

    Deletion added.

--- src/Bitap.java
+++ src/Bitap.java
@@ -2,8 +2,8 @@ public class Bitap
 {
     public static void main(String args[])
     {
-        String text = "AATCCG";
-        String pattern = "ATCCG";
+        String text = "CTC";
+        String pattern = "CTG";
         System.out.println("Text: " + text);
         System.out.println("Pattern: "+ pattern);
         search(text, pattern);
@@ -13,7 +13,7 @@ public class Bitap
     {
         int length = pattern.length() - 1;
 
-        long M = 1, S = 3, I = 1, vector[] = new long[256];
+        long M = 1, S = 1, I = 1, D = 3, vector[] = new long[256];
         int i, k;
 
         for(k = 0; k < 256; k++)
@@ -31,12 +31,15 @@ public class Bitap
             S = (((S << 1) | 1) & vector[text.charAt(i)]) | (M << 1);
             I = (((I << 1) | 1) & vector[text.charAt(i)]) | M;
             M = ((M << 1) | 1) & vector[text.charAt(i)];
+            D = ((M << 1) | 1) | (vector[text.charAt(i)] & (D << 1));
             if(M >= Math.pow(2, length + 1))
                 System.out.println("Full Match " + (i - length + 1));
             if(S >= Math.pow(2, length + 1))
                 System.out.println("Substitution " + (i - length + 1));
             if(I >= Math.pow(2, length + 1))
                 System.out.println("Insertion " + (i - length));
+            if(D >= Math.pow(2, length + 1))
+                System.out.println("Deletion " + (i - length + 2));
         }
     }
 }
